1c449313f4ddffc87e8d56830c2b3508912fe79b
Tori <torihendricks927@yahoo.com>
2022-05-02 18:35:20 -0400

    added template and questions

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ inquirer
         type: "license",
         message: "Which license do you wish to add to your ReadME?",
         name: "license",
+        choices: ["Apache", "MIT", "GNU"],
     },
     {
         type: "github",
@@ -50,5 +51,62 @@ inquirer
         type: "email",
         message: "What is your email address?",
         name: "email",
-    }
+    },
 ])
+.then((response) => {
+
+    const readmeTemp = `
+    # ${response.title}
+
+## Description
+${response.description}.
+
+## Installation
+${response.installation}.
+
+## Usage
+${response.usage}.
+
+## Contributing
+${response.contribution}.
+
+## Tests
+${response.test}.
+
+## License
+${response.license}.
+
+## Contact
+${response.github}.
+${response.email}.
+`
+})
+.then(function (response) {
+    for(let i = 0; i < response.license; i++) {
+        if (response.license === 0) {
+            ( `Copyright
+
+                Licensed under the Apache License, Version 2.0 (the "License");
+                you may not use this file except in compliance with the License.
+                You may obtain a copy of the License at
+             
+                  http://www.apache.org/licenses/LICENSE-2.0
+             
+                Unless required by applicable law or agreed to in writing, software
+                distributed under the License is distributed on an "AS IS" BASIS,
+                WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                See the License for the specific language governing permissions and
+                limitations under the License.`)
+        } else if (response.license === 1) {
+            (`Copyright
+
+                Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+                
+                The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+                
+                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`)
+        } else {
+            (``)
+        }
+    }
+})
