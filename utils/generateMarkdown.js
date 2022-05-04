3e6b673657965e3e85d88ff13be1bdb3e7bca685
Tori <torihendricks927@yahoo.com>
2022-05-03 21:44:29 -0400

    adding file provided for hw

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,44 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(response) {
+  return `# ${response.title}
+  
+  ## Description
+${response.description}.
+
+## Installation
+${response.installation}.
+
+## Usage
+${response.usage}.
+
+## Contributing
+${response.contribution}.
+
+## Tests
+${response.test}.
+
+## License
+${response.license}.
+
+## Questions
+For any additional questions, you can reach me at the following options:
+${response.github}.
+${response.email}.
+`;
+}
+
+module.exports = generateMarkdown;
